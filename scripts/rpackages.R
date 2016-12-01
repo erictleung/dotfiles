009fa236060cb3d29342a36c65e6d38ad51efb4a
Eric Leung <eric@erictleung.com>
2016-11-30 17:29:28 -0800

    Add rpackages.R script to download useful packages

---
+++ scripts/rpackages.R
@@ -0,0 +1,12 @@
+# Download useful packages
+# Last updated: 2016 Nov 30
+
+installPkges <- c("tidyverse",      # packages related to tidy data
+                  "knitr",          # create reproducible documents
+                  "rmarkdown",      # convert R Markdown documents
+                  "tables",         # help create publication tables
+                  "MASS",           # modern applied statistics functions
+                  "devtools"        # help create R packages
+)
+
+install.packages(installPkges)
