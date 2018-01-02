4ff56e61d7670a49324aa57db08bdb212752cbe0
Eric Leung <eric@erictleung.com>
2018-01-02 11:33:17 -0800

    emacs: rename .emacs to init.el
    Move the configuration file into the .emacs.d directory to consolidate
    all emacs files into one directory.
    
    Additionally, added back the menu for easier use in Linux, for now. Will
    probably remove later on when I get used to all the functionality of
    emacs.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,7 +20,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (## evil-visual-mark-mode))))
+ '(package-selected-packages (quote (## evil-visual-mark-mode)))
+ '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -97,7 +98,6 @@
 ;; remove unnecessary toolbars, scrollbars, etc
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; set up ace-jump-mode
 (add-to-list 'load-path "which-folder-ace-jump-mode-file-in/")
