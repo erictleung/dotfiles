f6fa568e93093c4b9507027531102f0a6c186736
Eric Leung <eric@erictleung.com>
2018-01-12 08:17:43 -0800

    scripts: add caret and Bioconductor to R packages

--- scripts/rpackages.R
+++ scripts/rpackages.R
@@ -1,12 +1,20 @@
 # Download useful packages
-# Last updated: 2016 Nov 30
+# Last updated: 2018-01-12
 
-installPkges <- c("tidyverse",      # packages related to tidy data
-                  "knitr",          # create reproducible documents
-                  "rmarkdown",      # convert R Markdown documents
-                  "tables",         # help create publication tables
-                  "MASS",           # modern applied statistics functions
-                  "devtools"        # help create R packages
+# install useful packages
+installPkges <- c(
+  "tidyverse",      # packages related to tidy data
+  "knitr",          # create reproducible documents
+  "rmarkdown",      # convert R Markdown documents
+  "tables",         # help create publication tables
+  "MASS",           # modern applied statistics functions
+  "devtools",       # help create R packages
+  "caret"           # powerful metapackage for predictive modeling
 )
 
 install.packages(installPkges)
+
+# install Bioconductor
+# source: https://www.bioconductor.org/install/
+source("https://bioconductor.org/biocLite.R")
+biocLite()
