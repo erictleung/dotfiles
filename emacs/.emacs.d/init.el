49c4712c66ee5d4b29320d6ba188d72549318991
Eric Leung <eric@erictleung.com>
2018-02-13 08:37:37 -0800

    emacs: add space to fix heading

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -177,6 +177,6 @@
      "Journal"
      entry
      (file+olp "~/Dropbox/org/journal.org")
-     "* %T\n**Grateful?\n*** %?\n** Making today great?\n** Daily affirmations. I am...\n** Three amazing things\n** How to make today better?"
+     "* %T\n** Grateful?\n*** %?\n** Making today great?\n** Daily affirmations. I am...\n** Three amazing things\n** How to make today better?"
     )
 )))
