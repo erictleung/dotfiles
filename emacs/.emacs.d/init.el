d2226156ca02a2e477f4445fa1636d2206104d01
Eric Leung <eric@erictleung.com>
2018-02-19 09:44:03 -0800

    emacs: add spaces and entries in journal

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -184,6 +184,6 @@
      "Journal"
      entry
      (file "~/Dropbox/org/journal.org")
-     "* %T\n** Grateful?\n*** %?\n** Making today great?\n** Daily affirmations. I am...\n** Three amazing things\n** How to make today better?"
+     "* %T\n** Grateful?\n*** %?\n** Making today great?\n** Daily affirmations. I am...\n** Three amazing things\n*** \n** How to make today better?\n*** "
     )
 )))
