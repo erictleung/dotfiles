bec5427ab8f6ede6305c7cbe11496d0a8d6fd200
Eric Leung <eric@erictleung.com>
2018-07-02 19:39:14 -0700

    emacs: specify auto-formatting of bibtex citations

--- emacs/.emacs.d/lisp/init-ref.el
+++ emacs/.emacs.d/lisp/init-ref.el
@@ -1,4 +1,8 @@
 ;; Org-Reference Configuration ;;
+;; Sources
+;; - https://github.com/jkitchin/org-ref/blob/master/org-ref.org
+;; - http://kitchingroup.cheme.cmu.edu/blog/2014/05/13/Using-org-ref-for-citations-and-references/
+;; - http://kitchingroup.cheme.cmu.edu/blog/2014/05/15/Using-org-ref-to-keep-your-bibtex-files-in-order/
 
 (require 'org-ref)
 (require 'interleave)
@@ -25,5 +29,14 @@
 ;; setup where PDFs can be found
 (setq bibtex-completion-library-path
       '("~/Dropbox/zotero"))
+
+;; setup auto-formatting of citation
+(setq bibtex-autokey-year-length 4
+      bibtex-autokey-name-year-separator ""
+      bibtex-autokey-year-title-separator "-"
+      bibtex-autokey-titleword-separator "-"
+      bibtex-autokey-titlewords 3
+      bibtex-autokey-titlewords-stretch 1
+      bibtex-autokey-titleword-length 15)
 
 (provide 'init-ref)
