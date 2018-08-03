4583fa6b3981059323be9d0b56721125f97460f6
Eric Leung <eric@erictleung.com>
2018-08-02 23:44:17 -0700

    emacs: add use-package for package management

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,9 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
 
+;; Inhibit startup screen
+(setq inhibit-startup-message t)
+
 ;; Packages I use
 (setq required-packages
       '(ido
@@ -25,14 +28,18 @@
 (require 'package)
 
 ;; Add in popular packages
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-
 (setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Try out package briefly before committing to them
+(use-package try
+  :ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
