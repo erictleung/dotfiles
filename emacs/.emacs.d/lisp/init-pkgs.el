6626ee16fc4cd11842805e0defcaadc69ed79a9d
Eric Leung <eric@erictleung.com>
2018-11-02 22:58:26 -0700

    emacs: improve writing with writegood-mode

--- emacs/.emacs.d/lisp/init-pkgs.el
+++ emacs/.emacs.d/lisp/init-pkgs.el
@@ -70,4 +70,9 @@
 (use-package xclip
   :ensure t)
 
+;; Improve writing with tips from
+;; http://matt.might.net/articles/shell-scripts-for-passive-voice-weasel-words-duplicates/
+(use-package writegood-mode
+  :ensure t)
+
 (provide 'init-pkgs)
