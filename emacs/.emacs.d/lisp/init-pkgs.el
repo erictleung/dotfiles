646df25a4c084ab73781e97a778c518aaf3771e7
Eric Leung <eric@erictleung.com>
2018-12-07 18:54:15 -0800

    emacs: reverse change on pdf-view-mode hook
    The previous configuration was correct.

--- emacs/.emacs.d/lisp/init-pkgs.el
+++ emacs/.emacs.d/lisp/init-pkgs.el
@@ -84,6 +84,6 @@
   (setq-default pdf-view-display-size 'fit-page))
 
 ;; Disable linum-mode in pdf-view-mode
-(add-hook 'doc-view-mode-hook (lambda () (linum-mode 0)))
+(add-hook 'pdf-view-mode-hook (lambda () (linum-mode 0)))
 
 (provide 'init-pkgs)
