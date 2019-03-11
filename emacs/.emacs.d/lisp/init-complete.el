6d1581835fd78c1069f963a81f3b755a5806014d
Eric Leung <eric@erictleung.com>
2019-03-10 22:05:47 -0700

    emacs: add more configs to company and quickhelp

--- emacs/.emacs.d/lisp/init-complete.el
+++ emacs/.emacs.d/lisp/init-complete.el
@@ -3,7 +3,21 @@
   :ensure t
   :init
   :config
+  (setq company-minimum-prefix-length 2)
+  (setq company-idle-delay 0)
+  (setq company-selection-wrap-around t)
   (global-company-mode t))
+
+;; More quick help
+(use-package company-quickhelp
+  :ensure t
+  :defer t
+  :disabled t
+  :commands company-quickhelp-mode
+  :init
+  (progn
+    (setq company-quickhelp-idle-delay 0.2)
+    (add-hook 'after-init-hook 'company-quickhelp-mode)))
 
 ;; Completions for academic phrases
 (use-package academic-phrases
