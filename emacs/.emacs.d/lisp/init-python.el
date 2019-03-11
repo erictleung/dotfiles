54c3f1620dbd87c2d5cf8dd4df9d9bd90afff265
Eric Leung <eric@erictleung.com>
2019-03-11 09:08:29 -0700

    emacs: add subpackages to config ein package

--- emacs/.emacs.d/lisp/init-python.el
+++ emacs/.emacs.d/lisp/init-python.el
@@ -21,9 +21,16 @@
   (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
 
 ;; Use IPython Notebooks/Jupyter in Emacs
+;; Src: https://github.com/millejoh/emacs-ipython-notebook
 ;; Docs: http://millejoh.github.io/emacs-ipython-notebook/
 (use-package ein
   :ensure t
-  :defer t)
+  :defer t
+  :config
+  ;; Stop ein from starting company-mode autocomplete
+  (add-hook 'ein:notebook-multilang-mode-hook '(lambda () (company-mode -1)))
+  (use-package ein-notebook)
+  (use-package ein-subpackages)
+  (use-package ein-jupyter))
 
 (provide 'init-python)
