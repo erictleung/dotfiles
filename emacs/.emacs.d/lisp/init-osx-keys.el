d768f2143e88735fed6d9d9d546b1b6fed2eaeae
Eric Leung <eric@erictleung.com>
2020-01-05 00:15:01 -0800

    emacs: remove duplicate and outdated configuration

--- emacs/.emacs.d/lisp/init-osx-keys.el
+++
@@ -1,7 +0,0 @@
-;; I prefer cmd key for meta
-(setq mac-option-key-is-meta nil
-      mac-command-key-is-meta t
-      mac-command-modifier 'meta
-      mac-option-modifier 'none)
-
-(provide 'init-osx-keys)
