3085213e3d2c95c67ef9be5c0f700983b0589192
Eric Leung <eric@erictleung.com>
2020-03-22 18:38:20 -0700

    scripts: format Ubuntu scripts files for reading

--- scripts/ubuntupkgs.sh
+++ scripts/ubuntupkgs.sh
@@ -5,4 +5,12 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install Ubuntu packages
-sudo apt-get install tmux htop ncdu diction vim emacs tree aspell pandoc
+sudo apt-get install \
+    tmux \ # Terminal multiplexer
+    htop ncdu \ # Nice GUI replacements
+    diction aspell \ # Help with writing
+    vim emacs \ # Editors
+    pandoc pandoc-citeproc \ # Pandoc and tools
+    stow \ # Configuration management
+    w3m \ # Command line web browser
+    tree # Misc tools
