1e30e6e3913e10585e4b3f15c09072fbacb6fd89
Eric Leung <eric@erictleung.com>
2020-03-22 18:29:43 -0700

    scripts: add core packages for Ubuntu

--- scripts/ubuntupkgs.sh
+++ scripts/ubuntupkgs.sh
@@ -5,4 +5,4 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install Ubuntu packages
-sudo apt-get install tmux htop ncdu diction
+sudo apt-get install tmux htop ncdu diction vim emacs tree aspell pandoc
