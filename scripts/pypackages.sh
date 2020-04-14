62e0d6f6355142fd8fdef92f47d5147dde409932
Eric Leung <eric@erictleung.com>
2020-04-14 13:48:19 -0700

    scripts: add Python package installation script

---
+++ scripts/pypackages.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install Python packages
+pip install \
+    ranger \            # Directory manager and navigation
+    proselint \         # Help with writing
+    nbdime              # Help diffing and merging of Jupyter notebooks
