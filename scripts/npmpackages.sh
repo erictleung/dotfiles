3f687c76512e09b9d2ed744dc8a53bb2b2c96c1f
Eric Leung <eric@erictleung.com>
2020-10-22 12:44:35 -0700

    scripts: setup non-sudo use of npm

--- scripts/npmpackages.sh
+++ scripts/npmpackages.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Setup by changing folders
+# https://stackoverflow.com/a/23889603/6873133
+npm config set prefix ~/.npm
+export PATH="$PATH:$HOME/.npm/bin"
+
 # npm packages
 npm install -g pandiff diff-so-fancy
