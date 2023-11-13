01ff8a56eaf8e462b512a907c06f99bb9e0f4589
Eric Leung <eric@erictleung.com>
2023-11-13 01:48:22 -0500

    crouton: create flatpak.sh for desktop apps

---
+++ crouton/flatpak.sh
@@ -0,0 +1,17 @@
+# https://flathub.org/apps/me.kozec.syncthingtk
+flatpak install flathub me.kozec.syncthingtk
+
+# https://flathub.org/apps/com.visualstudio.code
+flatpak install flathub com.visualstudio.code
+
+# https://flathub.org/apps/org.gimp.GIMP
+flatpak install flathub org.gimp.GIMP
+
+# https://flathub.org/apps/org.libreoffice.LibreOffice
+flatpak install flathub org.libreoffice.LibreOffice
+
+# https://flathub.org/apps/md.obsidian.Obsidian
+flatpak install flathub md.obsidian.Obsidian
+
+# https://flathub.org/apps/org.blender.Blender
+flatpak install flathub org.blender.Blender
